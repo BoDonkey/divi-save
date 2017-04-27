516707e8fd391a00771c7002b5311f7d074b36e5
BoDonkey <robert.means1969@gmail.com>
2017-04-27 10:03:43 -0400

    Initial files added and README.md updated

---
+++ button-add.php
@@ -0,0 +1,29 @@
+<?php
+//add button to admin bar
+function custom_button_example($wp_admin_bar){
+	$args = array(
+		'id' => 'shd-divi-save-button',
+		'title' => 'Save',
+		'meta' => array(
+			'class' => 'shd-button-class--save',
+			)
+		);
+	$wp_admin_bar->add_node($args);
+}
+add_action('admin_bar_menu', 'custom_button_example', 1500);
+
+//add script for button function
+function visual_button() {
+	wp_enqueue_script( 'button-query', get_stylesheet_directory_uri() . '/js/visual.save.js', array( 'jquery' ), null, false );
+}
+
+//slot styling for button into admin pages - note, must also add to the stylesheet for it to carry to the non-admin pages
+function admin_button_styling() {
+	echo ' <style>
+#wpadminbar li.shd-button-class--save {
+	display: none;
+	background-color: green;
+}
+</style>';
+}
+add_action('admin_head', 'admin_button_styling');
