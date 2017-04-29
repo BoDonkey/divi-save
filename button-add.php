28520db90a05c812542e81d7cc1edcccd564d76d
BoDonkey <robert.means1969@gmail.com>
2017-04-29 07:39:15 -0400

    Modifies the add-button.php file to use better function names

--- button-add.php
+++ button-add.php
@@ -1,6 +1,6 @@
 <?php
 //add button to admin bar
-function shd_custom_button_example( $wp_admin_bar ) {
+function shd_add_divi_fb_save_button( $wp_admin_bar ) {
 	$args = array(
 		'id' => 'shd-divi-save-button',
 		'title' => 'Save',
@@ -10,19 +10,20 @@ function shd_custom_button_example( $wp_admin_bar ) {
 		);
 	$wp_admin_bar->add_node($args);
 }
-add_action( 'admin_bar_menu', 'custom_button_example', 1500 );
+add_action( 'admin_bar_menu', 'shd_add_divi_fb_save_button', 1500 );
 
 //add script for button function
-function visual_button() {
+function shd_divi_save_button() {
 	wp_enqueue_script( 'button-query', get_stylesheet_directory_uri() . '/js/visual.save.js', array( 'jquery' ), null, false );
 }
+add_action( 'wp_enqueue_scripts', 'shd_divi_save_button' );
 
 //slot styling for button into admin pages - note, must also add to the stylesheet for it to carry to the non-admin pages - I also changed the background-color: green; to make more visible
-function admin_button_styling() {
+function shd_divi_save_button_styling() {
 	echo ' <style>
 #wpadminbar li.shd-button-class--save {
 	display: none;
 }
 </style>';
 }
-add_action( 'admin_head', 'admin_button_styling' );
+add_action( 'admin_head', 'shd_divi_save_button_styling' );
